ae1a6fc591595ada4eb1d81159fe37bdcae1c068
George Walker <george@escapesystems.com>
2016-11-03 10:19:56 -0700

    Update dockerfile

---
+++ SampleNetCore/src/SampleNetCore/wwwroot/app.js
@@ -0,0 +1,16 @@
+﻿var React = require('react');
+
+var App = React.createClass({
+    render(){
+
+        return (
+
+			    <div>
+				Hello from React.
+                </div>
+
+            );
+    }
+});
+
+module.exports = App;
