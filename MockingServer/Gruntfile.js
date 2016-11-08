09fbc812f796dbac54a366bd853964253adaa95d
George Walker <george@escapesystems.com>
2016-11-08 15:55:54 -0800

    Testing mocking server

---
+++ MockingServer/Gruntfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var path = require('path');
+var mockApi = require('swagger-mock-api');
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+		  port: 80,
+          keepalive: true,
+          middleware: [
+            mockApi({
+                  //swaggerFile: path.join(__dirname, 'C:\Users\georg\Source\Repos\nov16test\SampleNetCore\src\SampleNetCore\Swagger\sample.json'),
+                  swaggerFile: 'C:\\Users\\georg\\Source\\Repos\\nov16test\\SampleNetCore\\src\\SampleNetCore\\Swagger\\sample.json',
+                  
+				  watch: true // enable reloading the routes and schemas when the swagger file changes
+              })
+          ],
+        },
+      },
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // Default task(s).
+  grunt.registerTask('default', ['connect']);
+
+};
