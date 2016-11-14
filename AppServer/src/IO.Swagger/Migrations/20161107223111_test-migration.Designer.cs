b18f431950e5c8a664823b593bf12b386bf46ee1
George Walker <george@escapesystems.com>
2016-11-14 10:59:54 -0800

    Move app server

---
+++ AppServer/src/IO.Swagger/Migrations/20161107223111_test-migration.Designer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using IO.Swagger.Models;
+
+namespace IO.Swagger.Migrations
+{
+    [DbContext(typeof(DbTestContext))]
+    [Migration("20161107223111_test-migration")]
+    partial class testmigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1");
+
+            modelBuilder.Entity("IO.Swagger.Models.Vehicle", b =>
+                {
+                    b.Property<int?>("VehicleId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("DisplayName");
+
+                    b.HasKey("VehicleId");
+
+                    b.ToTable("Vehicles");
+                });
+        }
+    }
+}
